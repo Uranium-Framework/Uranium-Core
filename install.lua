404af008ed69d6dd8a6beabafebc7bc3dabe6b22
Syn <99213873+SyntalDev@users.noreply.github.com>
2022-03-06 21:43:17 +0000

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,1 +1,10 @@
-print("Hi")
+local HttpService = game:GetService("HttpService")
+local Replicate = game:GetService("ReplicatedStorage")
+
+
+--################ Structuring ################--
+
+local psScript = Instance.new("Script", game:GetService("ServerScriptService"))
+local expan = Instance.new("Folder", psScript)
+local setting = Instance.new("ModuleScript", psScript)
+setting.source = "Hi"
