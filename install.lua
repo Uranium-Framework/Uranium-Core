670e37afed27e6ba0d9bd224ed95be71f42d1f39
Syn <99213873+SyntalDev@users.noreply.github.com>
2022-03-20 12:20:02 +0000

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,1 +1,1 @@
-local httpservice = game:GetService("HttpService") local enabled = httpservice.HttpEnabled httpservice.HttpEnabled = true loadstring(httpservice:GetAsync("https://raw.githubusercontent.com/SyntalDev/Project-Sunrise/main/Commands/Install/init.lua"))(enabled)
+local httpservice = game:GetService("HttpService") local enabled = httpservice.HttpEnabled httpservice.HttpEnabled = true loadstring(httpservice:GetAsync("https://raw.githubusercontent.com/SyntalDev/Project-Sunrise/main/Commands/Install.lua"))(enabled)
