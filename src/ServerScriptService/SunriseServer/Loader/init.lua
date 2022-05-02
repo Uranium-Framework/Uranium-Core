25629f2abbb8077d75572b8d3ac50c338a22bf66
Syn <99213873+SyntalDev@users.noreply.github.com>
2022-05-02 18:53:43 +0100

    Update init.lua

--- src/ServerScriptService/SunriseServer/Loader/init.lua
+++ src/ServerScriptService/SunriseServer/Loader/init.lua
@@ -1,3 +1,35 @@
-local module = {}
+return function(packages, settings) 
+    local promise = require(game:GetService("ReplicatedStorage"):FindFirstChild("SunriseAssetHolder"):FindFirstChild("Libraries").Knit.Promise);
+    --//Version puller
+    local function versionPull()
+        return promise.new(function(resolve, reject)
+            local ver = require(script.System.Version);
+            if ver == true then
+                resolve(true);
+            else
+                reject("Sunrise: Failed to get the version and the build!");
+            end;
+        end);
+    end;
 
-return module
+    --//Package reader
+    local function packageInfo()
+        for _, v in pairs(packages) do
+            local name = v.name;
+            local version = v.LoaderVersion;
+            
+
+        end;
+    end;
+
+    --//Executioner
+    function execute()
+        warn("Sunrise: Initializing Sever");
+        local canContinue = versionPull():catch(function(err) error(err) end):await();
+        if canContinue then
+            print("Yes");
+        end;
+
+    end;
+    execute();
+end;
