fbb54608fc9a1cda51fc5090a9478af78e90c72e
Syn <99213873+SyntalDev@users.noreply.github.com>
2022-05-02 15:21:07 +0100

    Delete install.lua

--- install.lua
+++
@@ -1,1 +0,0 @@
-local httpservice = game:GetService("HttpService") local enabled = httpservice.HttpEnabled httpservice.HttpEnabled = true loadstring(httpservice:GetAsync("https://raw.githubusercontent.com/SyntalDev/Project-Sunrise/main/Commands/Install.lua"))(enabled)
