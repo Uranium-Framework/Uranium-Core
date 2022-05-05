5159cd727248be6c3ed01d20382a3add6d8a82f9
Syn <99213873+SyntalDev@users.noreply.github.com>
2022-05-05 21:57:30 +0100

    Update init.lua

--- src/ServerScriptService/SunriseServer/Loader/init.lua
+++ src/ServerScriptService/SunriseServer/Loader/init.lua
@@ -1,35 +1,72 @@
 return function(packages, settings) 
+    --//Variables
     local promise = require(game:GetService("ReplicatedStorage"):FindFirstChild("SunriseAssetHolder"):FindFirstChild("Libraries").Knit.Promise);
-    --//Version puller
-    local function versionPull()
-        return promise.new(function(resolve, reject)
-            local ver = require(script.System.Version);
-            if ver == true then
-                resolve(true);
-            else
-                reject("Sunrise: Failed to get the version and the build!");
-            end;
-        end);
+   local packageInfo = {};
+   
+   --//Version puller(Gets the current version of Sunrise)
+    local function versionPull(resolve, reject)
+        local ver = require(script.System.Version);
+        if ver then
+            resolve(ver);
+        else
+            reject("Version was not obtained")
+        end;
     end;
 
-    --//Package reader
-    local function packageInfo()
+    --//Expansion reader(Reads the expansions and saves them to packageInfo)
+    local function expansionInfo()
         for _, v in pairs(packages) do
-            local name = v.name;
-            local version = v.LoaderVersion;
-            
+            local scriptName = v.name; 
+            local required = require(v);
+            if table.find(packageInfo, scriptName) == nil then
+                packageInfo[scriptName] = {required.Name, required.LoaderVersion}; 
+            end;
+            return required.LoaderVersion, v, required;
+        end;
+    end;
+
+
+    --//Expansion executor(Runs all the expansions and imports all the important things into the expansion)
+    local function expansionExe(expansion)
+        if expansion.Execute then
+            expansion.API = require(script.System.SunriseAPI)
+
+            --//Extension reader
+            for extension, value in pairs(require(script.Parent.Settings).extends) do
+                if game:GetService("ReplicatedStorage"):FindFirstChild("Packages", true):FindFirstChild(extension) then
+                    if value == true then
+                        for extensionIn, val in pairs(expansion.Extends) do
+                            if extension == extensionIn and val == true then
+                                expansion.Extends[extension] = require(game:GetService("ReplicatedStorage"):FindFirstChild("Packages", true):FindFirstChild(extension))
+                            end;
+                        end;
+                    else
+                        warn(string.format("The extension %s", extension, "%s is inactive!"))
+                    end;
+                else
+                    warn(string.format("There is no extension with the name %s", extension))
+                end;
+            end;
+            --//End of the extension reader
 
         end;
     end;
 
-    --//Executioner
+    --//Executioner(Runs all of the functions)
     function execute()
         warn("Sunrise: Initializing Sever");
-        local canContinue = versionPull():catch(function(err) error(err) end):await();
-        if canContinue then
-            print("Yes");
-        end;
-
+        local verPull = promise.new(versionPull);
+        verPull:andThen(function(arg1)
+            local ver, package, req = expansionInfo();
+            if ver ~= arg1 then
+                package.Parent = game:GetService("ReplicatedStorage"):FindFirstChild("AssetContainment", true);
+                warn("Sunrise: The expansion", package.name, "is outdated. Consider changing the expansions version to", arg1, "This expansion will be placed in AssetContainment!")
+            else
+                expansionExe(req)
+            end;
+        end)
+        :catch(function(err) error(err) end)
     end;
     execute();
 end;
+
